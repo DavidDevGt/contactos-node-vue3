2104777e652482263493843efcf32443b33432e0
DavidDevGt <josuedavidvl18@gmail.com>
2023-10-12 21:50:32 -0600

    cambio en la UI

--- frontend/src/components/ContactList.vue
+++ frontend/src/components/ContactList.vue
@@ -1,13 +1,23 @@
 <template>
-  <div>
-    <h1>Lista de Contactos</h1>
-    <ul v-if="contacts.length > 0">
-      <li v-for="contact in contacts" key="contact.id">
-        {{ contact.name }} - {{ contact.phone }}
-      </li>
-    </ul>
-    <p v-else>No hay contactos disponibles.</p>
-    <p v-if="error">{{ error }}</p>
+  <div class="container py-5">
+    <h1 class="text-center mb-4">Lista de Contactos</h1>
+    
+    <div v-if="contacts.length > 0" class="row">
+      <div v-for="contact in contacts" :key="contact.id" class="col-md-4 mb-4">
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title">{{ contact.name }}</h5>
+            <p class="card-text">
+              <i class="bi bi-telephone-fill"></i>
+              {{ contact.phone }}
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <p v-else class="text-center text-muted">No hay contactos disponibles.</p>	
+    <p v-if="error" class="text-center text-danger">{{ error }}</p>
   </div>
 </template>
 
@@ -34,17 +44,4 @@ export default {
 </script>
 
 <style>
-  h1 {
-    color: #333;
-    text-align: center;
-  }
-
-  ul {
-    list-style-type: none;
-    padding: 0;
-  }
-
-  p {
-    color: red;
-  }
 </style>
